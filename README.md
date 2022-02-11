0430f50cf87bd4ff32fdeac47474236b6fa98376
Christopher Barbas <chrisbarbas30@gmail.com>
2022-02-11 14:02:15 -0500

    working version

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # library
-library project from The Odin Project
+library from The Odin Project 
+https://www.theodinproject.com/paths/full-stack-javascript/courses/javascript/lessons/library
+
+Live Demo: https://chrisbarbas.github.io/library/
+
+![library](https://i.imgur.com/a2LVxwL.png)
--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let myLibrary = [];
-
 const form = document.querySelector('.book-form');
 const book = document.querySelector('#book');
 const author = document.querySelector('#author');
@@ -7,42 +5,20 @@ const pages = document.querySelector('#pages');
 const read = document.querySelector('#read');
 const table = document.querySelector('.table');
 
-form.addEventListener('submit', (e) => {
-    clearForm();
-    checkInputs();
-    buildTable();
-    e.preventDefault();
-});
+//form -> validate -> book constructor -> array -> table-builder
+let myLibrary = [{
+    name: 'The Hobbit',
+    author: 'J.R.R. Tolkien',
+    pages: 310,
+    read: 'read'
+}];
 
-
-const checkInputs = () => {
-    const bookValue = book.value.trim();
-    const authorValue = author.value.trim();
-    const pagesValue = pages.value.trim();
-    const readValue = read.value.trim();
-    console.log(readValue)
-    if (bookValue === '') {
-        setErrorFor(book);
-    }
-    if (authorValue === '') {
-        setErrorFor(author);
-    }
-    if (pagesValue === '') {
-        setErrorFor(pages);
-    }
-    myLibrary.unshift(new Book(bookValue, authorValue, pagesValue, readValue))
+let library = JSON.parse(localStorage.getItem('library'));
+if (JSON.parse(localStorage.getItem('library') != null)) {
+    myLibrary = library;
 }
 
-const setErrorFor = (input) => {
-    const formItem = input.parentElement;
-    formItem.className = 'form-item error';
-}
-
-const clearForm = () => {
-    const formItem = document.querySelectorAll('.form-item');
-    formItem.className = ('form-item');
-}
-
+//book constructor for myLibrary
 class Book {
     constructor(name, author, pages, read) {
         this.name = name;
@@ -52,32 +28,68 @@ class Book {
     }
 }
 
+//accepts form info and sends to array
+const checkInputs = () => {
+    let messages = []
+    const bookValue = book.value.trim();
+    const authorValue = author.value.trim();
+    const pagesValue = pages.value.trim();
+    const readValue = read.value.trim();
+    if (bookValue === '') {
+        setErrorFor(book);
+        messages.push('enter a book');
+    } else {
+        setSuccessFor(book)
+    }
+    if (authorValue === '') {
+        setErrorFor(author);
+        messages.push('enter an author');
+    } else {
+        setSuccessFor(author)
+    }
+    if (pagesValue === '') {
+        setErrorFor(pages);
+        messages.push('enter an author');
+    } else {
+        setSuccessFor(pages)
+    }
+    if (messages.length > 0) {
+        return false;
+    } else {
+        myLibrary.unshift(new Book(bookValue, authorValue, pagesValue, readValue));
+        updateLocal();
+        return true;
+    }
+}
+
+//builds the table looping the array
 const buildTable = () => {
+    //delete extra rows
+    while (table.rows.length > 0) {
+        table.deleteRow(0);
+    }
     myLibrary.forEach((lib, index) => {
-        //delete extra rows
-        if (index > 0) {
-            table.deleteRow(0);
-        }
         //row
         let row = document.createElement('tr');
         row.dataset.index = index;
-        //readbtn
+        //read button
         let readbtn = document.createElement('button');
         let readStatus = Object.values(lib).pop();
-        if (readStatus === 'read') {
-            readbtn.classList.add("read");
+        readbtn.textContent = readStatus;
+        readbtn.className = 'read';
+        if (readStatus === 'not read') {
+            readbtn.className = 'not-read';
         }
-        else if (readStatus === 'not read') {
-            readbtn.classList.add("not-read");
-        }
-        console.log(lib.read)
-        //delbtn
+        readbtn.addEventListener('click', readCheck);
+        // delete button
         let delbtn = document.createElement('button');
         delbtn.textContent = 'delete';
-        //items within the rows
+        delbtn.classList.add('delete-toggle');
+        delbtn.addEventListener('click', deleteCheck);
+        //removes read text from array
         let newArray = Object.values(lib).slice(0, 3);
+        //loops through the objects
         newArray.forEach(text => {
-            // Object.values(lib).forEach(text => {
             let cell = document.createElement('td');
             let textNode = document.createTextNode(text);
             cell.appendChild(textNode);
@@ -86,6 +98,59 @@ const buildTable = () => {
         row.append(readbtn);
         row.append(delbtn);
         table.appendChild(row);
-        console.log(row.dataset.index);
     });
 };
+
+//submit button
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (checkInputs()) { //if true
+        buildTable();
+        form.reset();
+    }
+});
+
+//reverses class name, text, and array>object.read
+function readCheck(e) {
+    const item = e.target;
+    let bookIndex = item.parentElement.dataset.index;
+    let readClass = item.className;
+    if (readClass === 'read') {
+        console.log('red');
+        item.className = 'not-read';
+        item.textContent = 'not read';
+        myLibrary[bookIndex].read = 'not read';
+        updateLocal();
+    } else {
+        console.log('blue');
+        item.className = 'read';
+        item.textContent = 'read';
+        myLibrary[bookIndex].read = 'read';
+        updateLocal();
+    }
+}
+
+//deletes the parent row and removes from array
+function deleteCheck(e) {
+    const item = e.target;
+    let bookIndex = item.parentElement.dataset.index;
+    item.parentElement.remove();
+    myLibrary.splice(bookIndex, 1);
+    updateLocal();
+}
+
+const setErrorFor = (input) => {
+    const formItem = input.parentElement;
+    formItem.className = 'form-item error';
+}
+
+const setSuccessFor = (input) => {
+    const formItem = input.parentElement;
+    formItem.className = 'form-item success';
+}
+
+function updateLocal() {
+    localStorage.setItem('library', JSON.stringify(myLibrary));
+}
+
+buildTable();
